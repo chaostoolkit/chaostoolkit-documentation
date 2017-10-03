b85530c133d021d3a5d60cc69924c11d4070c8d7
Sylvain Hellegouarch <sh@defuze.org>
2017-10-03 11:02:08 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# chaostoolkit-documentation
-The Chaos Toolkit documentation
+# The Chaos Toolkit documentation
+
+This project is based on [MkDocs][], a Python documentation generator based on
+[Markdown][].
+
+To get started, install Python 3 on your machine, a [virtual environment][venv]
+and install the dependencies using [pip][] as follows:
+
+```
+$ pip install -r requirements.txt
+```
+
+[MkDocs]: http://www.mkdocs.org/
+[Markdown]: https://daringfireball.net/projects/markdown/syntax
+[venv]: https://virtualenv.pypa.io/en/stable/
+[pip]: https://pip.pypa.io/en/stable/installing/
+
+Once the dependencies are installed, you can start a local server to view the
+docs:
+
+```
+$ mkdocs serve
+```
+
+If you wish to contribute to this documentation, please submit a PR with your
+changes for review.
---
+++ ci.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -eo pipefail
+
+function build-docs () {
+    echo "Building the documentation"
+    mkdir /tmp/site
+    cd /tmp/site
+    git clone https://$GH_USER_NAME:$GH_USER_PWD@github.com/chaostoolkit-documentation/chaostoolkit-documentation.git .
+    git checkout gh-pages
+    cd -
+    mkdocs build --strict -d /tmp/site
+}
+
+function publish-docs () {
+    echo "Publishing the documentation"
+    cd /tmp/site
+    echo "chaostoolkit.org" > CNAME
+    git add .
+    git commit -a -m "Built from ${TRAVIS_COMMIT}"
+    git push
+}
+
+function main () {
+    build-docs || return 1
+
+    if [[ "$TRAVIS_BRANCH" == "master" ]] && [[ "$TRAVIS_PULL_REQUEST" == false ]]; then
+        # build docs on each commit but only from master
+        publish-docs || return 1
+    fi
+}
+
+main "$@" || exit 1
+exit 0

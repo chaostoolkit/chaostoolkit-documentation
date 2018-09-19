a5102925ed652ff6d28e024dfdeb289673624980
Sylvain Hellegouarch <sh@defuze.org>
2018-09-19 16:53:08 +0200

    Add dependency
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- ci.bash
+++ ci.bash
@@ -11,7 +11,7 @@ function fetch-and-install-chaostoolkit-packages() {
         --no-deps \
         --no-cache-dir \
         --dest deps \
-        --no-binary=chaostoolkit-aws,chaostoolkit-azure,chaostoolkit-cloud-foundry,chaostoolkit-google-cloud,chaostoolkit-humio,chaostoolkit-kubernetes,chaostoolkit-prometheus,chaostoolkit-spring \
+        --no-binary=chaostoolkit-aws,chaostoolkit-azure,chaostoolkit-cloud-foundry,chaostoolkit-google-cloud,chaostoolkit-humio,chaostoolkit-kubernetes,chaostoolkit-prometheus,chaostoolkit-spring,chaostoolkit-toxiproxy \
         -r requirements-toolkit.txt
 
     cd deps
